4fb452984f7747e4981bc85ab08f6728a13da138
Aida Ospanova <ospanovaida08@gmail.com>
2019-12-11 23:28:42 +0600

    Add labs

--- week9/project/asbolsyn/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
-# router.register('tasks', TaskViewSet, base_name='core')
-
-urlpatterns = router.urls
--- week9/project/asbolsyn/utils/upload.py
+++
@@ -1,3 +0,0 @@
-def profile_image_path(instance, filename):
-    user_id = instance.user.id
-    return f'images/profile/{user_id}/{filename}'
